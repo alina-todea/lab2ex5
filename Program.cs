74a9db9059a7da5adfd97a4a346b4ce52839af1b
alina-todea <137332096+alina-todea@users.noreply.github.com>
2023-07-01 15:37:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab2ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             Scrieti o functie care afisa piramida numerelor pare, intre 2 si n, n fiind citit de la tastatura
+             */
+            Console.WriteLine("Introduceti un numar");
+            int n = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 2; i <=n; i+=2)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
